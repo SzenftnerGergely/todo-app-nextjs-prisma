8a8c136e1fb721bf2ab71553ef065b54cc5034c1
SzenftnerGergely <gergely.szenftner@gmail.com>
2024-01-07 17:36:41 +0100

    map structure created, prisma and mongodb connection set up, input comp created

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,9 +1,27 @@
-const Input = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface inputProps {
+  name: string;
+  type: string;
+  placeholder?: string;
+  value?: string;
 }
 
-export default Input
+const Input = ({
+  name,
+  type,
+  placeholder,
+  value,
+}: inputProps) => {
+  return (
+    <>
+      <input
+        className="w-full p-2 border-gray-200 border"
+        name={name}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+      />
+    </>
+  );
+};
+
+export default Input;
